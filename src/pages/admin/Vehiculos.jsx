2faf4f95ad4c5dbc6fa83a9d50726557463d4c0a
Juan Carlos Martinez Burr <jcmb2049@gmail.com>
2021-12-01 22:20:20 -0500

    agregar ejemplo de renderizacion condicional

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -2,47 +2,66 @@ import React, {useEffect, useState} from 'react';
 
 const Vehiculos = () => {
     
-    const[nombreVehiculo, setNombreVehiculo] = useState('Nombre inicial del input');
+    const [edad, setEdad] = useState(0);
+    const [esMenorDeEdad, setEsMenorDeEdad] = useState(false);
+    const[mostrarCamposAdicionales,setMostrarCamposAdicionales] = useState(false);
 
-    useEffect(() => {   
-
-        console.log('Hola, soy un use effect que se ejecuta solo una vez cuando la pagina se renderiza, por que tiene el array de dependencias vacio');
-        //paso 2
-        //paso 3
-        //paso 4
-    },[]);
-
-    useEffect(() =>{
-        console.log('esto es una funcion que se ejecuta cada que cambia el valor de nombreVehiculo')
-        console.log('el valor de la variable nombreVehiculo es: ', nombreVehiculo)
-    },[nombreVehiculo]);
-
-   // useEffect(() =>{
-    //    console.log('este es un use effect que se ejecuta Siempre que cambia una variable')
-    //})
-
-    const enviarDatosAlBackend = (e) => {
-        console.log('El valor de la variable nombreVehiculo es:', nombreVehiculo);
-    }
+    useEffect(()=>{
+        if(edad >= 18){
+            setEsMenorDeEdad(false);
+        } else{
+            setEsMenorDeEdad(true);
+        }
+    },[edad])
 
     return (
         <form className='flex flex-col'>
             <h2>Formulario de creacion de vehiculos</h2>
-            <input onChange={(e) =>{
-                setNombreVehiculo(e.target.value);
-            }} 
-            value={nombreVehiculo}
-            type='text' 
-            placeholder='Nombre del vehiculo' />
-            <input onChange={(e) =>{
-                console.log(e.target.value);
-            }} 
-            type="text" 
-            placeholder='Marca del vehiculo'/>
-            <input type="text" placeholder='Modelo del vehiculo' />
-            <button type='button' onClick={enviarDatosAlBackend} className='bg-indigo-500 text-white' >Enviar Datos</button>
+            <label htmlFor="edad">
+                Por favor ingrese su edad
+                <input value={edad} 
+                onChange={(e) => {
+                    setEdad(e.target.value)}}
+                    className='border border-gray-600' 
+                    name='edad'
+                    type="number" />
+            </label>
+            {esMenorDeEdad ? (
+                <span className='text-3xl text-red-500'>
+                !Usted es menor de edad, no puede hacer pagos¡
+            </span>
+            ) : (
+                <span className='text-3xl text-green-500'>
+                !Usted es mayor de edad, si puede hacer pagos¡
+            </span>
+            )} 
+            <button type='button' onClick={() => { setMostrarCamposAdicionales(!mostrarCamposAdicionales)}} className='bg-indigo-500 text-white'>Mostrar campos adicionales</button>           
+            {mostrarCamposAdicionales && (
+                <div>
+                    <input className='border bg-gray-400 m-2 p-3' placeholder='Dato Nuevo' type="text" />
+                    <input className='border bg-gray-400 m-2 p-3' placeholder='Dato Nuevo' type="text" />
+                    <input className='border bg-gray-400 m-2 p-3' placeholder='Dato Nuevo' type="text" />
+                    <input className='border bg-gray-400 m-2 p-3' placeholder='Dato Nuevo' type="text" />
+                    <input className='border bg-gray-400 m-2 p-3' placeholder='Dato Nuevo' type="text" />
+                </div>
+            )}
         </form>
     )
 }
+
+/*if(esMenorDeEdad){
+    return(
+        <span className='text-3xl text-red-500'>
+                !Usted es menor de edad, no puede hacer pagos¡
+            </span>
+    )
+    
+} else{
+    return(
+        <span className='text-3xl text-green-500'>
+                !Usted es mayor de edad, si puede hacer pagos¡
+            </span>
+    )
+}*/
 
 export default Vehiculos
